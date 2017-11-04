849a0808ee54e59ce4a02717c35ac145c9a7203f
daraeman <daraeman@gmail.com>
2017-11-04 14:24:09 -0700

    remove debug code

--- server/controller/Feed.js
+++ server/controller/Feed.js
@@ -17,17 +17,6 @@ const retrieveFeed = function( feed ) {
 
 		let date = new Date();
 
-		console.log( "path", createSavePath( feed.path ) + "/" + "1509817749960.json" )
-
-		fs.readFile( createSavePath( feed.path ) + "/" + "1509817749960.json" )
-			.then( ( body ) => {
-
-				return resolve( body );
-			})
-			.catch( ( error ) => {
-				return reject( error );
-			});
-/*	
 		let json;
 		request( feed.url )
 			.then( ( body ) => {
@@ -48,7 +37,6 @@ const retrieveFeed = function( feed ) {
 				feed.save();
 				return reject( error );
 			});
-*/
 	});
 };
 
