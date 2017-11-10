c9efa750d76790d69b90d7798c97417d0bf5adc2
daraeman <daraeman@gmail.com>
2017-11-09 18:27:54 -0800

    create sidestats component

--- client/components/Hero.js
+++ client/components/Hero.js
@@ -2,6 +2,7 @@ import React from "react"
 import { connect } from "react-redux"
 import LineGraph from "./LineGraph"
 import Jumbotron from "./Jumbotron"
+import SideStats from "./SideStats"
 
 import { fetchHero } from "../actions/Hero"
 
@@ -65,14 +66,45 @@ export default class Hero extends React.Component {
 
 		const { hero } = this.props;
 
+		let side_stats_data = [
+							{ title: "Introduced", text: "1 November 2016" },
+							{ title: "Skins", list: {
+								type: "link",
+								items: [
+									{ link: "#", text: "Skin 1" },
+									{ link: "#", text: "Skin 2" },
+									{ link: "#", text: "Skin 3" },
+									{ link: "#", text: "Skin 4" },
+									{ link: "#", text: "Skin 5" },
+								]
+							} }
+						];
+
 		return (
 
 			<main role="main">
 
 				<Jumbotron title={ "Hero: "+ hero.name }></Jumbotron>
 
-				<LineGraph data={ this.state.ice } id="graph_ice" name="ice"></LineGraph>
-				<LineGraph data={ this.state.glory } id="graph_glory" name="glory"></LineGraph>
+				<div class="container">
+
+					<div class="row">
+
+						<div class="col-xs-2">
+
+							<h2>Ice</h2>
+							<LineGraph data={ this.state.ice } id="graph_ice" name="ice"></LineGraph>
+
+							<h2>Glory</h2>
+							<LineGraph data={ this.state.glory } id="graph_glory" name="glory"></LineGraph>
+
+						</div>
+
+						<SideStats title="Stats" data={ side_stats_data }></SideStats>
+
+					</div>
+
+				</div>
 
 			</main>
 		)
