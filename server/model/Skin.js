d16c49e2eb7d09bd73703396e79786dd0b078dab
daraeman <daraeman@gmail.com>
2017-11-13 15:45:17 -0800

    add hero id to skin model

--- server/model/Skin.js
+++ server/model/Skin.js
@@ -1,8 +1,10 @@
 const mongoose = require( "mongoose" );
 const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
 
 const SkinSchema = new Schema({
 	symbol: { type: String },
+	hero: { type: ObjectId },
 });
 
 module.exports = mongoose.model( "Skin", SkinSchema );
